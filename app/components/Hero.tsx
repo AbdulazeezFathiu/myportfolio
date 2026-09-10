01479ee1eabdb16eda88f5e74aded9becfbca823
Fathiu <fathiuabdulazeez@gmail.com>
2026-09-10 20:42:07 +0100

    Replace project with new version

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ import { profile } from "@/lib/data";
 
 export default function Hero() {
   return (
-    <section id="top" className="flex min-h-[70svh] flex-col justify-center">
+    <section id="top" className="flex min-h-[40svh] flex-col justify-center">
       <h1 className="text-4xl font-semibold tracking-tight text-zinc-900 sm:text-5xl">
         {profile.name}
       </h1>
